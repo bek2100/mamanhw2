064e86f67f3f3377733e0b6d9f05c3db3a98165c
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-19 16:12:24 +0300

    try1

--- wet.c
+++ wet.c
@@ -31,11 +31,6 @@ int main(int argc, char** argv) {
     res = PQexec(conn, cmd);
 
     
-    sprintf(cmd, "DROP TABLE Account;");
-    
-    res = PQexec(conn, cmd);
-    
-    
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "1Error executing query: %s\n", PQresultErrorMessage(res)); return 1; }
     
     sprintf(cmd, "CREATE TABLE Account AS SELECT * FROM course_Account;");
