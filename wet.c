46f5385ffc3625d4940945deebd0effe19bc25cf
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-19 15:59:32 +0300

    try1

--- wet.c
+++ wet.c
@@ -26,27 +26,27 @@ int main(int argc, char** argv) {
     
     char cmd[CMD_SIZE];
     
-    sprintf(cmd, "DROP TABLE Account ""CREATE TABLE tableName AS SELECT * FROM course_Account;");
+    sprintf(cmd, "DROP TABLE Account; ""CREATE TABLE Account AS SELECT * FROM course_Account;");
     
     res = PQexec(conn, cmd);
     
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return 1; }
-    sprintf(cmd, "DROP TABLE OwnsAcc ""CREATE TABLE tableName AS SELECT * FROM course_OwnsAcc;");
+    sprintf(cmd, "DROP TABLE OwnsAcc; ""CREATE TABLE OwnsAcc AS SELECT * FROM course_OwnsAcc;");
     
     res = PQexec(conn, cmd);
     
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return 1; }
-    sprintf(cmd, "DROP TABLE Withdrawal ""CREATE TABLE tableName AS SELECT * FROM course_Withdrawal;");
+    sprintf(cmd, "DROP TABLE Withdrawal; ""CREATE TABLE Withdrawal AS SELECT * FROM course_Withdrawal;");
     
     res = PQexec(conn, cmd);
     
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return 1; }
-    sprintf(cmd, "DROP TABLE tableName ""CREATE TABLE ManagesAcc AS SELECT * FROM course_ManagesAcc;");
+    sprintf(cmd, "DROP TABLE ManagesAcc; ""CREATE TABLE ManagesAcc AS SELECT * FROM course_ManagesAcc;");
     
     res = PQexec(conn, cmd);
     
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return 1; }
-    sprintf(cmd, "DROP TABLE Transfer ""CREATE TABLE tableName AS SELECT * FROM course_Transfer;");
+    sprintf(cmd, "DROP TABLE Transfer; ""CREATE TABLE Transfer AS SELECT * FROM course_Transfer;");
     
     res = PQexec(conn, cmd);
     
