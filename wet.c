b04b0476e4b66e723223de253090776c28c66696
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-19 12:36:55 +0300

    try

--- wet.c
+++ wet.c
@@ -97,7 +97,7 @@ void* addAccount(int ANumber, int ID, int BrNumber) {
     
     PQclear(res);
     
-    sprintf(cmd, "INSERT INTO Account "" VALUES(%d, 0, -1000)", ANumber);
+    sprintf(cmd, "INSERT INTO Account "" VALUES(%d, %d, %d)", ANumber, 0, -1000);
     
     res = PQexec(conn, cmd);
     
