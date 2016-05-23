2274649659f0054ba05ccd36d99adbe3ff866066
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-23 23:32:08 +0300

    try

--- wet.c
+++ wet.c
@@ -652,18 +652,23 @@ void* moneyLaundering() {
     
     char cmd[CMD_SIZE];
     
-    sprintf(cmd, "CREATE VIEW Money AS SELECT IDF, IDT, TAmount FROM Transfer");
+    sprintf(cmd, "CREATE VIEW Money0 AS SELECT IDF, IDT, TAmount FROM Transfer");
     res = PQexec(conn, cmd);
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
     
     int num_id = PQntuples(res);
+    int i=1;
     
     while (num_id){
-     sprintf(cmd, "CREATE VIEW Money AS SELECT * FROM MONEY "" UNION ALL "" SELECT T.IDF, T1.IDT, T1.TAmount FROM Money T, T1 WHERE T.IDT=T1.IDF AND T.TAmount>=T1.TAmount");
+     sprintf(cmd, "CREATE VIEW Money%d AS SELECT * FROM MONEY "" UNION ALL "" SELECT T.IDF, T1.IDT, T1.TAmount FROM Money T, T1 WHERE T.IDT=T1.IDF AND T.TAmount>=T1.TAmount", i);
      
      res = PQexec(conn, cmd);
      
      if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
+    
+        sprintf(cmd, "DROP VIEW Money%d",i-1);
+        res = PQexec(conn, cmd);
+        if(!res) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
         
         num_id--;
     }
@@ -672,7 +677,6 @@ void* moneyLaundering() {
     res = PQexec(conn, cmd);
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
 
-    sprintf(cmd, "DROP VIEW Money");
     res = PQexec(conn, cmd);
     
      int t_num = PQntuples(res);
@@ -684,8 +688,12 @@ void* moneyLaundering() {
      for(i=0; i<t_num;i++)
      printf("%d\n", PQgetvalue(res, i, 0));
      }
-     
-     PQclear(res);
+    
+    PQclear(res);
+    sprintf(cmd, "DROP VIEW Money%d",i);
+    res = PQexec(conn, cmd);
+    if(!res) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
+
     return NULL;
 }
 
