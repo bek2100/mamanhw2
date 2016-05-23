44e18e1401a5953928c1d571012655103196c815
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-23 17:26:52 +0300

    try1

--- wet.c
+++ wet.c
@@ -392,8 +392,7 @@ void* transfer(double TAmount, int IDF, int ANumberF, int IDT, int ANumberT) {
     
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
     
-    int  i = 0;
-    if (ANumberF>ANumberT) i=1;
+    int  i = (ANumberF>ANumberT)? 1:0;
     int j = 1 -i;
     
     double BalanceF = atof(PQgetvalue(res, i, 1)) - (TCommission + TAmount);
