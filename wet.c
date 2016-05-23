5d580d304d060f371ecff59b24b0d1ad74e1593e
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-23 18:18:40 +0300

    try1

--- wet.c
+++ wet.c
@@ -551,7 +551,7 @@ void* balances(int ID, int ANumber) {
     else {
         printf(BALANCES_HEADER);
         for(i = 0; i < t_num; i++){
-            Diff-=atof(PQgetvalue(res, i, 5));
+            Diff+=atof(PQgetvalue(res, i, 5));
             if(atof(PQgetvalue(res, i, 4)))
                 printf(CREDIT_RESULT, PQgetvalue(res, i, 0), atof(PQgetvalue(res, i, 1)), Diff);
             else printf(DEBIT_RESULT, PQgetvalue(res, i, 0), atof(PQgetvalue(res, i, 1)), atof(PQgetvalue(res, i, 2)), Diff);
