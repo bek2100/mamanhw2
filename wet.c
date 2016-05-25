6ceb15e6ef77e1ad3b852c50ee129a7581e0869e
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-25 20:53:32 +0300

    test

--- wet.c
+++ wet.c
@@ -655,7 +655,7 @@ void* moneyLaundering() {
     if(!res) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
     
     
-    sprintf(cmd, "CREATE TABLE money AS SELECT TID,TAmount,IDF,IDT, TAmount AS PATH FROM Transfer; SELECT * FROM money");
+    sprintf(cmd, "CREATE TABLE money AS SELECT TID,TAmount,IDF,IDT FROM Transfer; SELECT * FROM money");
     res = PQexec(conn, cmd);
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
     
@@ -676,7 +676,7 @@ void* moneyLaundering() {
    while (num_id){
     PQclear(res);
         
-       sprintf(cmd, "INSERT INTO money (TID, TAmount, IDF, IDT, PATH) SELECT T1.TID, T1.TAmount, T.IDF, T1.IDT, CONCAT(T.PATH, ' ' ,T1.PATH) FROM money T INNER JOIN money T1 ON T.IDT=T1.IDF AND T1.TAmount<=T.TAmount AND T.TID<T1.TID");
+       sprintf(cmd, "INSERT INTO money (TID, TAmount, IDF, IDT) SELECT T1.TID, T1.TAmount, T.IDF, T1.IDT,  FROM money T INNER JOIN money T1 ON T.IDT=T1.IDF AND T1.TAmount<=T.TAmount AND T.TID<T1.TID");
        
      res = PQexec(conn, cmd);
      
