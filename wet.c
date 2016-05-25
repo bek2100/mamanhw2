b6ad5151f3a25f355a80ea0199062dee671f822e
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-25 14:56:12 +0300

    test

--- wet.c
+++ wet.c
@@ -392,13 +392,13 @@ void* transfer(double TAmount, int IDF, int ANumberF, int IDT, int ANumberT) {
     
     PQclear(res);
     
-    sprintf(cmd, "SELECT * FROM Account WHERE ANumber=%d OR ANumber=%d ORDER BY ANnumber", ANumberT, ANumberF);
+    sprintf(cmd, "SELECT * FROM Account WHERE ANumber=%d OR ANumber=%d ORDER BY ANumber", ANumberT, ANumberF);
     
     res = PQexec(conn, cmd);
     
     if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
     
-    int i = (ANumberT>ANumberF)? 0:1;
+    int i = (ANumberT>ANumberF)? 1:0;
     int j = 1-i;
     
 
