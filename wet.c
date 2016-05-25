b1fc031143fda72cf910a8d687f0b1b416328697
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-25 13:21:21 +0300

    try3

--- wet.c
+++ wet.c
@@ -649,7 +649,7 @@ void* moneyLaundering() {
     
     printf(MONEY_LAUNDERING);
     
-    /*char cmd[CMD_SIZE];
+    char cmd[CMD_SIZE];
     
    int i=0;
     
@@ -675,7 +675,7 @@ void* moneyLaundering() {
      if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "2Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
         
         num_id--;
-    }
+    }*/
     
     PQclear(res);
     
@@ -701,7 +701,7 @@ void* moneyLaundering() {
     
     if(!res) { fprintf(stderr, "3Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
 
-    PQclear(res);*/
+    PQclear(res);
     return NULL;
 }
 
