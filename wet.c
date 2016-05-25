d95c2687b71cb986c3dc3114f962620898a16c40
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-25 13:37:25 +0300

    try3

--- wet.c
+++ wet.c
@@ -669,7 +669,7 @@ void* moneyLaundering() {
     
     res = PQexec(conn, cmd);
     
-    if(!res) { fprintf(stderr, "2Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
+    if(!res || PQresultStatus(res) != PGRES_TUPLES_OK) { fprintf(stderr, "2Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
     
    /*while (num_id){
         PQclear(res);
