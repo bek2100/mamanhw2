696634f114c8a49a7a28bfe48ce79070102aa97a
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-25 21:55:19 +0300

    test

--- wet.c
+++ wet.c
@@ -675,9 +675,12 @@ void* moneyLaundering() {
     
    while (num_id){
     PQclear(res);
-        
-       sprintf(cmd, "INSERT INTO money (TID, TAmount, IDF, IDT) SELECT T1.TID, T1.TAmount, T.IDF, T1.IDT FROM money T LEFT JOIN money T1 ON T.IDT=T1.IDF AND T1.TAmount<=T.TAmount AND T.TID<T1.TID AND T.IDT<>T.IDF AND T1.IDT<>T1.IDF AND NOT EXISTS(SELECT * FROM money M WHERE T1.TID=M.TID T1.TAmount=M.TAmount AND T.IDF=M.IDF AND T1.IDT=M.IDT)");
-       
+
+    
+    sprintf(cmd, "INSERT INTO money (TID, IDF, IDT, TAmount) SELECT T1.TID, T.IDF, T1.IDT, T1.TAmount FROM money T INNER JOIN money T1 ON T.IDT=T1.IDF AND T.TAmount>=T1.TAmount AND T.TID<T1.TID AND NOT EXISTS(SELECT * FROM money M WHERE T1.TID=M.TID AND T.IDF=M.IDF AND T1.IDT=M.IDT AND T1.TAmount=M.TAmount)");
+
+
+
      res = PQexec(conn, cmd);
      
      if(!res) { fprintf(stderr, "Error executing query: %s\n", PQresultErrorMessage(res)); return NULL; }
