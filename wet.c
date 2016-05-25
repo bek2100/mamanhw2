cfef1c4d7c0801c3b2eb2a3818f868e5d66cf462
רבקה בר-סף <rebeccoosh@gmail.com>
2016-05-25 16:00:58 +0300

    test

--- wet.c
+++ wet.c
@@ -608,7 +608,7 @@ void* associates(int ID) {
     
     PQclear(res);
     
-    sprintf(cmd, "SELECT IDF FROM ((SELECT IDF FROM Transfer WHERE IDT = (SELECT IDT FROM Transfer WHERE IDF =%d OR IDT =%d) AS B OR IDT = %d) UNION (SELECT IDT FROM Transfer WHERE IDF = (SELECT IDF FROM Transfer WHERE IDT =%d OR IDF =%d) OR IDF = %d)) AS A ORDER BY IDF", ID, ID, ID, ID, ID, ID);
+    sprintf(cmd, "SELECT IDF WHERE IDT=%d UNION SELECT IDT WHERE IDF=%d", ID, ID);
     
     res = PQexec(conn, cmd);
     
